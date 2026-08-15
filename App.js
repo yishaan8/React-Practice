e60aa9dabfc2d75fb8909b57a52aba17e1d87b9a
yishaan8 <yishaan8@gmail.com>
2026-08-15 13:39:42 +0530

    some work

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},[React.createElement("h1",{},"Hello from h1 tag"),React.createElement("h2",{},"hello from h2 tag")])
+)
+
+
 const heading = React.createElement("h1",{id:"heading"},"hello from react");
     
 const root= ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
