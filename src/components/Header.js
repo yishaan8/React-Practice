83e9a951c5228e14f351eab01e62dfb37ff7ac06
yishaan8 <yishaan8@gmail.com>
2026-08-23 18:15:56 +0530

    implemented client side routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState ,useEffect } from "react";
-
+import { Link } from "react-router-dom";
 const Header = () => {
 
   const [btnNameReact,setbtnNameReact] = useState("Login")
@@ -22,9 +22,11 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button className="login" onClick={()=>{
             btnNameReact==="Login" ? setbtnNameReact("Logout") : setbtnNameReact("Login");
