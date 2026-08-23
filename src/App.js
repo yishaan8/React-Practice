08b351f7fc0019286cc9014239c29357cf750bc0
yishaan8 <yishaan8@gmail.com>
2026-08-23 14:39:07 +0530

    implementing contact us page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import  Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 
 const AppLayout = () => {
@@ -22,6 +23,10 @@ const appRouter=createBrowserRouter([
   {
     path:"/about",
     element:<About />
+  },
+  {
+    path:"/contact",
+    element:<Contact />
   }
 ])
 
