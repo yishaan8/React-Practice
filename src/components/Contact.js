343707c4cd55ec993056d5020d6cb50898cfeec3
yishaan8 <yishaan8@gmail.com>
2026-08-29 23:13:55 +0530

    improved contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,11 @@ const Contact=()=>{
     return(
         <div>
             <h1>Contact</h1>
+            <h2>Work Email</h2>
+            <p>john.doe@company.com</p>
+            <h2>Personal Email</h2>
+            <p>john.doe@gmail.com</p>
+            <h2>Phone</h2>
         </div>
     )
 }
