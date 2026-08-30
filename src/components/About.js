bf8191d5a0c66c850ba6706212e66ad74e170293
yishaan8 <yishaan8@gmail.com>
2026-08-30 22:52:08 +0530

    Updated about us

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ const About=()=>{
     return(
         <div>
             <h1>About Us</h1>
-            <h2>This is React</h2>
+            <h2>This is Demo project by Ishaan Yadav</h2>
         </div>
     )
 }
