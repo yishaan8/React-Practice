a3a9de848b0dcd783d3031210470700c00047df7
yishaan8 <yishaan8@gmail.com>
2026-09-05 23:57:49 +0530

    changes in shimmer ui

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -18,6 +18,7 @@ const Shimmer=()=>{
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
     </div>
 }
 
